54213bd4ace563799c5f8fdcb981ff144d677a70
Fedor Kovalev <kovalevfm@gmail.com>
2014-07-31 11:58:17 +0400

    zoo partitioner with set in zookeeper, uses kazoo, kafka batch producer

---
+++ kafka_batch_producer.py
@@ -0,0 +1,90 @@
+from __future__ import absolute_import
+
+import logging
+from kafka.common import ProduceRequest
+from kafka.partitioner import HashedPartitioner
+from kafka.protocol import create_message
+
+log = logging.getLogger("kafka")
+
+BATCH_SEND_DEFAULT_INTERVAL = 20
+BATCH_SEND_MSG_COUNT = 20
+
+STOP_ASYNC_PRODUCER = -1
+
+
+class BatchProducer(object):
+    """
+    A producer which distributes messages to partitions based on the key
+    and send batches
+
+    Params:
+    client - The Kafka client instance to use
+    partitioner - A partitioner class that will be used to get the partition
+        to send the message to. Must be derived from Partitioner
+    req_acks - A value indicating the acknowledgements that the server must
+               receive before responding to the request
+    ack_timeout - Value (in milliseconds) indicating a timeout for waiting
+                  for an acknowledgement
+    """
+
+    ACK_NOT_REQUIRED = 0            # No ack is required
+    ACK_AFTER_LOCAL_WRITE = 1       # Send response after it is written to log
+    ACK_AFTER_CLUSTER_COMMIT = -1   # Send response after data is committed
+
+    DEFAULT_ACK_TIMEOUT = 2000
+    DEFAULT_MESSAGE_LIMIT = 1000000
+
+    def __init__(self, client, partitioner=None,
+                 req_acks=ACK_AFTER_CLUSTER_COMMIT,
+                 ack_timeout=DEFAULT_ACK_TIMEOUT,
+                 message_limit=DEFAULT_MESSAGE_LIMIT):
+
+        self.client = client
+        self.req_acks = req_acks
+        self.ack_timeout = ack_timeout
+        self.message_limit = message_limit
+        self.partitioner_class = partitioner or HashedPartitioner
+        self.partitioners = {}
+
+    def _next_partition(self, topic, key):
+        if topic not in self.partitioners:
+            if topic not in self.client.topic_partitions:
+                self.client.load_metadata_for_topics(topic)
+            self.partitioners[topic] = \
+                self.partitioner_class(self.client.topic_partitions[topic])
+        partitioner = self.partitioners[topic]
+        return partitioner.partition(key, self.client.topic_partitions[topic])
+
+    def send_messages(self, topic, key_msg_list):
+        """
+        Helper method to send produce requests
+        """
+        reqs = []
+        sub_reqs = []
+        msg_size = 0
+        for key, msg in key_msg_list:
+            req = ProduceRequest(topic,
+                                 self._next_partition(topic, key),
+                                 [create_message(msg)])
+            if msg_size + len(req.messages[0]) < self.message_limit:
+                sub_reqs.append(req)
+                msg_size += len(req.messages)
+            else:
+                reqs.append(sub_reqs)
+                sub_reqs = [req]
+                msg_size = len(req.messages)
+        else:
+            reqs.append(sub_reqs)
+        try:
+            resps = []
+            for sub_reqs in reqs:
+                resps.extend(self.client.send_produce_request(sub_reqs, acks=self.req_acks,
+                                                              timeout=self.ack_timeout))
+        except Exception:
+            log.exception("Unable to send messages")
+            raise
+        if not all(resp.error == 0 for resp in resps):
+            log.exception("response has errors {0}".format(resps))
+            raise
+        return resps
